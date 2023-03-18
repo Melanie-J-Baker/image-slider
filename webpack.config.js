734ef02b7b51fef0cb3089cb9450e87f387b2b95
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-18 06:34:19 +0000

    Add webpack, loaders and plugins and configure

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            { test: /\.txt$/, use: 'raw-loader' },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Image Slider',
+            template : __dirname + '/dist/index.html',
+            filename : 'index.html',
+            inject : 'body'
+        }),
+    ],
+};
