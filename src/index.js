5989e800d318fee59c45453b4074c6c2fbbc8316
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-18 07:54:31 +0000

    Create functions to switch between slides and add event listeners to buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-import './style.css'
+import "./style.css";
+
+const slides = document.querySelectorAll(".slide");
+
+slides.forEach((slide, index) => {
+  slide.style.transform = `translateX(${index * 100}%)`;
+});
+
+let currentSlide = 0;
+let maxSlide = slides.length - 1;
+const nextSlide = document.querySelector(".right-arrow");
+
+nextSlide.addEventListener("click", function () {
+  if (currentSlide === maxSlide) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+  });
+});
+
+const prevSlide = document.querySelector(".left-arrow");
+
+prevSlide.addEventListener("click", function () {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide;
+  } else {
+    currentSlide--;
+  }
+
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+  });
+});
